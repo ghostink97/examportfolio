bb09039fda7ffa802b7a3eb63d49061ba9677e60
ourangelofsorrows <angelezrameyer@gmail.com>
2018-12-27 18:17:44 +0100

    i finished the mobile version of basic content. I'm proud of my work

---
+++ js/portfolio.js
@@ -0,0 +1,7 @@
+let button=document.querySelector("#music");
+let sound=document.querySelector(".sound");
+
+button.addEventListener("click", playmusic);
+function playmusic(){
+    sound.play();
+}
