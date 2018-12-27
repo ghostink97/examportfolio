1f9c9a5f82909b88485942322544484dc4e1e38a
ourangelofsorrows <angelezrameyer@gmail.com>
2018-12-27 22:45:07 +0100

    js for basic animation isnt workiinggg

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,7 +1,14 @@
 let button=document.querySelector("#music");
 let sound=document.querySelector(".sound");
+let lola=document.querySelector("#lola");
 
 button.addEventListener("click", playmusic);
 function playmusic(){
     sound.play();
+}
+
+lola.addEventListener("click", lolaclicked);
+
+function lolaclicked(){
+    lola.classList.add("clicky");
 }
