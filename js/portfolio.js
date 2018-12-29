31cdcd94f87d0130eca8c2951886ad3bec158d80
ourangelofsorrows <angelezrameyer@gmail.com>
2018-12-29 12:14:31 +0100

    woo got the animation to work!

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,13 +2,15 @@ let button=document.querySelector("#music");
 let sound=document.querySelector(".sound");
 let lola=document.querySelector("#lola");
 
+lola.addEventListener("click", PlayLola);
+
+function PlayLola(){
+    lola.classList.toggle("clicked");
+}
+
+
 button.addEventListener("click", playmusic);
 function playmusic(){
     sound.play();
 }
 
-lola.addEventListener("click", lolaclicked);
-
-function lolaclicked(){
-    lola.classList.add("clicky");
-}
