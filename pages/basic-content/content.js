8edd0f5ae03a27df2642b3380c6e6c2b06282c4e
ourangelofsorrows <angelezrameyer@gmail.com>
2019-01-04 10:55:47 +0100

    some last changes to js and the pages. I think im done woo!

--- pages/basic-content/content.js
+++ pages/basic-content/content.js
@@ -1,9 +1,16 @@
 let button=document.querySelector("#music");
 let sound=document.querySelector(".sound");
+let soundPlaying = false;
 
-
-button.addEventListener("click", playmusic);
-function playmusic(){
+button.addEventListener("click", controlMusic);
+function controlMusic(){
+    if (soundPlaying == false){
     sound.play();
+    soundPlaying = true;
+    }
+    else{
+        sound.pause();
+        soundPlaying = false;
+    }
 }
 /*I'll ask julio on how to toggle the sound because GRADES*/
